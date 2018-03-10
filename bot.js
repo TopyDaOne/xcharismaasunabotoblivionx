903d192785788998330ea2c86eacbc277fcaec3a
maybecharisma <36387595+maybecharisma@users.noreply.github.com>
2018-03-10 11:34:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "!!";
-const Canvas = require('canvas')
 const jimp = require('jimp')
 const moment = require('moment')
 const ms = require("ms");
@@ -748,38 +747,7 @@ client.on('message', message => {
 
 
 
-client.on('message', message => {
-    var prefix = "!!"
-    let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
 
-  let args = message.content.split(" ").slice(1);
-
-
-if(command == "draw") {
-    var Canvas = require('canvas')
-  , Image = new Canvas.Image
-  , canvas = new Canvas(450, 170)
-  , ctx = canvas.getContext('2d');
-  ctx.font = '30px Impact';
-  let args = message.content.split(" ").slice(1);
-  
-Image.src = canvas.toBuffer();
-
-    console.log(Image);
-ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
-ctx.fillText(args.join("  "),110, 70);
-
-
-ctx.beginPath();
-ctx.lineTo(50, 102);
-ctx.stroke();
-
-message.channel.sendFile(canvas.toBuffer());
-}
-}).on('ready', () => {
-
-});
 
 client.on("message", (message) => {
     if (message.content.startsWith('!!delete')) {
